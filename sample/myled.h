257df08f5756d349b34d3bb64298d93434bb8a20
Rajesh Sola <rsola@gitam.edu>
2025-07-28 22:20:23 +0530

    Create myled.h

---
+++ sample/myled.h
@@ -0,0 +1,12 @@
+#ifndef __LED_H
+#define __LED_H
+
+#include<stdint.h>
+
+void initialize(uint8_t pin);
+void turnOn(uint8_t pin);
+void turnOff(uint8_t pin);
+void toggle(uint8_t pin);
+bool status(uint8_t pin);
+
+#endif
